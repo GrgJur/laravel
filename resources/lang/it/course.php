3c590c81d275af87898b996ee68889ef1c4e5877
Jure Grgic <jure.grgic@samtrevano.ch>
2021-02-04 14:00:22 +0100

    Aggiunta la cartella per la traduzione del linguaggio

---
+++ resources/lang/it/course.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'delete'=> 'Cancella il corso e tutte le sue lezioni',
+    'deleted'=> 'Il corso e tutte le sue lezioni sono stati cancellati correttamente',
+    'create'=> 'Aggiungi corso',
+    'type'=> 'Tipo di corso',
+    'facebook'=> 'Facebook',
+    'added'=> 'Corso inserito correttamente',
+    'no_lesson'=> 'al momento nessuna lezione',
+    'number'=> 'Nr. corso',
+
+
+];
---
+++ resources/lang/it/instructor.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+
+    'instructor'=> 'MC',
+    'current' => 'Istruttore corrente',
+    'delete' => 'Cancella istruttore',
+    'add' => 'Aggiungi istruttore',
+    'email'=> 'E-mail',
+    'firstname'=> 'Nome',
+    'lastname'=> 'Cognome',
+    'fullname'=> 'Istruttore',
+    'title'=> 'Titolo',
+    'address'=> 'Indirizzo',
+    'zip'=> 'NAP',
+    'city'=> 'Città',
+    'phone'=> 'Tel.',
+    'private'=> 'Tel. privato',
+    'mobile'=> 'Cellulare',
+    'work'=> 'Tel. ufficio',
+    'birthdate'=> 'Nascita',
+    'instructor_id'=> 'id istruttore',
+    'user_status_id'=> 'id stato',
+    'session'=> 'sessione',
+    'mr'=> 'Signor',
+    'ms'=> 'Signora',
+    'id'=> 'id',
+    'update'=> 'modifica',
+    'sure_to_delete'=> 'Sicuro di volerlo eliminare?',
+    'updated'=> 'Istruttore modificato correttamente',
+    'added'=> 'Istruttore aggiunto correttamente',
+    'deleted'=> 'Istruttore eliminato correttamente',
+    'search'=> 'Cerca...',
+    'show'=> 'Dettagli',
+    'edit'=> 'Modifica istruttore',
+    'detail'=> 'Dettaglio istruttore',
+    'details'=> 'Anagrafica',
+
+
+
+
+];
